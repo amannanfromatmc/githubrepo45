3bf97d66dbb4fa5efec6c21c3598c1f8e10f1d0f
Simon Kaegi <simon.kaegi@gmail.com>
2016-04-18 16:26:06 -0400

    bad commit on purpose

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,31 @@
 #!/bin/bash
-cf push node-hello-simon-devarun
+
+if [ $# -ne 1 ]; then
+	echo "Usage:\n\n\t./deploy.sh <appname>\n"
+	exit 1
+fi
+CF_APP_NAME=$1
+
+if ! cf app $CF_APP_NAME; then  
+  cf push $CF_APP_NAME
+else
+  rollback() {
+    set +e	
+    if cf app $OLD_CF_APP_NAME; then
+      cf logs $CF_APP_NAME --recent
+      cf delete $CF_APP_NAME -f
+      cf rename $OLD_CF_APP_NAME $CF_APP_NAME
+    fi
+    exit 1
+  }
+  OLD_CF_APP_NAME=${CF_APP_NAME}-OLD-$(date +"%s")
+  set -e
+  trap rollback ERR
+  cf rename $CF_APP_NAME $OLD_CF_APP_NAME
+  cf push $CF_APP_NAME
+  cf delete $OLD_CF_APP_NAME -f
+fi
+
+# View logs
+#cf logs "${CF_APP_NAME}" --recent
+
